d4c6dda3f8df723ed01d72853bf1142858a69725
Wind <zhangyf@phxg.cn>
2023-02-09 13:00:02 +0800

    chore: support jsx

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
+import VueJsx from '@vitejs/plugin-vue-jsx'
 import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -21,6 +22,7 @@ export default defineConfig({
         vue: Vue({
           reactivityTransform: true,
         }),
+        vueJsx: VueJsx(),
       },
     }),
 
